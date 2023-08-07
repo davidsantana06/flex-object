669ab78e9af2a88f700415012efa6f724e74280c
davidsantana06 <davidsantana06@hotmail.com>
2023-08-07 14:09:07 -0300

    feat: add flex obj class base

--- src/flex_object.py
+++ src/flex_object.py
@@ -1,2 +1,33 @@
-class FlexObject(object):
-    pass
+from types import MethodType
+
+
+class FlexObject():
+    def __init__(self, **attrs) -> None:
+        self.set_attrs(attrs)
+
+    def set_attrs(self, **attrs) -> None:
+        for name, value in attrs.items():
+            if name.startswith('_'):
+                continue
+            else:
+                if callable(value):
+                    value = MethodType(value, self)
+
+                setattr(self, name, value)
+
+    def del_attrs(self, *attr_names):
+        for name in attr_names:
+            if not type(name) == str:
+                continue
+            else:
+                if hasattr(self, name):
+                    del getattr(self, name)
+
+    def write_excel(self, file_name: str = None, folder_path: str = None, *args, **kwargs) -> None:
+        pass
+
+    def write_json(self, file_name: str = None, folder_path: str = None, *args, **kwargs) -> None:
+        pass
+
+    def write_txt(self, file_name: str = None, folder_path: str = None, *args, **kwargs) -> None:
+        pass
