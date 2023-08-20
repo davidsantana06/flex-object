c489d89ad4335aebde82835126c7d5f680efe564
davidsantana06 <davidsantana06@hotmail.com>
2023-08-20 14:09:56 -0300

    feat: add method to serialize object attributes as JSON

--- src/flex_object.py
+++ src/flex_object.py
@@ -1,12 +1,14 @@
 from typing import Any, Dict
 from types import MethodType
+from os.path import abspath, dirname, join
+import json
 
 
-class FlexObject():
+class FlexObject(object):
     def __init__(self, **attrs) -> None:
         self.set_attrs(**attrs)
 
-    def all_vars_attrs(self) -> Dict[str, Any]:
+    def all_var_attrs(self) -> Dict[str, Any]:
         return {
             name: value
             for name, value in self.__dict__.items()
@@ -34,5 +36,12 @@ class FlexObject():
     def to_excel(self, file_path: str = '') -> None:
         pass
 
-    def to_json(self, file_path: str = '') -> None:
-        pass
+    def to_json(self, file_path: str = '', indent: int = 4) -> None:
+        if not file_path:
+            file_path = join(
+                abspath(dirname(__file__)),
+                f'{self.__class__.__name__}.json'
+            )
+
+        with open(file_path, "w") as json_file:
+            json.dump(self.all_var_attrs(), json_file, indent=indent)
